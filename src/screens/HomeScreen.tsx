f2e52a00eb791bc9f2a0803f77c742e7617f6c9f
Javier Granados <hola@javiergranados.dev>
2022-05-30 20:20:33 +0200

    Install and implement the library react-native-vector-icons

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const HomeScreen = () => {
   return (
     <View>
       <Text>Home screen</Text>
+      <Icon name="home-outline" size={100} />
     </View>
   );
 };
